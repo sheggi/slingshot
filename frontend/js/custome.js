a409135353448eee55921154fcfb5c8899604774
sheggi <thesheggi@gmail.com>
2014-11-13 21:46:23 +0100

    tried to abstract with factorys

--- frontend/js/custome.js
+++ frontend/js/custome.js
@@ -1,5 +1,103 @@
 (function(){
-var app = angular.module('money', ['ui.bootstrap']);
+var model = angular.module("model", []);
+model.factory('Record', ['$http' ,function($http){
+	var find = function(data){ //FIXXX complete
+		var list = [ { "id": "1h989", "account_id": "a01", "amount": "100", "datetime": "2014-11-09T12:13:15+0100", "hint": "Start Saldo" }, { "id": "1lo86", "account_id": "a01", "amount": "-50", "datetime": "2014-11-09T13:45:12+0100", "hint": "Bezug" }, { "id": "19shh", "account_id": "a01", "amount": "60", "datetime": "2014-11-09T13:45:13+0100", "hint": "Einzahlung" }, { "id": "1QP7z", "account_id": "a01", "amount": "234", "datetime": "2014-11-13T14:02:49+0100", "hint": "asdf" }, { "id": "1TScg", "account_id": "a01", "amount": "233456", "datetime": "2014-11-13T16:40:29+0100", "hint": "asss" }, { "id": "1nGFD", "account_id": "a01", "amount": "-6544", "datetime": "2014-11-13T16:40:42+0100", "hint": "fsdg" } ];
+
+		return list;
+	}
+	
+	var Model = function() {
+		var id = "",
+		account_id = "",
+		datetime = "",
+		hint = "",
+		amount = 0;
+		var find = this.find;
+	};
+	
+	return {
+		Model: Model,
+		find: find
+	}
+}])
+model.factory('Account', ['$http', 'Record' ,function($http, Record){
+	var current = null;
+	
+	var find = function(data){ //FIXXX complete
+		var account = new Model();
+		account.id = "a01";
+		account.title = "Portemonaie";
+		account.records = Record.find({account_id:account.id});
+		return [account];
+	}
+	
+	var Model = function() {
+		var id = "",
+		nick = "",
+		forename = "",
+		surname = "",
+		email = "",
+		password = "",
+		additional = "",
+		records = [];
+		var find = this.find;
+	};
+	
+	return {
+		current: current,
+		Model: Model,
+		find: find
+	}
+}]);
+model.factory('User', ['$http', 'Account' ,function($http, Account){
+	var current = null;
+	
+	
+	var find = function(data){ //FIXXX complete
+		var user = new Model();
+		user.id = "u01";
+		user.nick = "admin";
+		user.accounts = Account.find({user_id:user.id});//FIXXX C //[{id:"a01", user_id:"u01", title:"Portemonaie"}];
+		return [user];
+	}
+	
+	var Model = function() {
+		var id = "",
+		user_id = "",
+		title = "",
+		description = "",
+		find = this.find;
+	};
+	
+	var selectAccount = function (identifier){
+		//FIXXX improve
+		console.log(this.current.accounts);
+		Account.current = this.current.accounts[identifier];
+	}
+	
+	return {
+		current: current,
+		Model: Model,
+		find: find,
+		selectAccount: selectAccount
+	}
+}]);
+model.factory('Session',
+['$http', 'User' ,function($http, User){
+
+	var logonUser = function (nick, password){
+		//FIXXX do login
+		User.current = User.find({nick:nick, password:password})[0];
+		return User.current;
+	}
+
+	return {
+		logonUser: logonUser
+	}
+}]);
+
+
 
 var compareDatetime = function (a, b) {
 	if (a.datetime > b.datetime) {
@@ -12,8 +110,20 @@ var compareDatetime = function (a, b) {
 	return 0;
 };
 
-	app.controller('MoneyController', ['$scope', '$http', '$filter', function($scope, $http, $filter){
+
+
+angular.module('money', ['ui.bootstrap', 'model']).controller('MoneyController',
+['$scope', '$http', '$filter', 'Session', 'User', 'Account', function($scope, $http, $filter, Session, User, Account){
 	$scope.records = [];
+	
+	Session.logonUser();
+	User.selectAccount(0);
+	
+	$scope.session = {};
+	$scope.session.user = User.current;
+	$scope.session.account = Account.current; 
+	console.log($scope.session);
+	
 	
 	req_data = {c:"Money", f:"getRecordList", a: {accountId:"a01"}};
 	$http.post("http://localhost/slingshot/backend/handle.php", req_data).
@@ -21,7 +131,7 @@ var compareDatetime = function (a, b) {
 		//alert(data || status || "success");
 		
 		$scope.records = data.response;
-		console.log(data.log);
+		console.log(data.response);
 		$scope.records.sort(compareDatetime);
 		
 	}).
@@ -48,16 +158,7 @@ var compareDatetime = function (a, b) {
 		success( function(data, status){
 			
 			$scope.records = data.response;
-			$scope.records.sort(function (a, b) {
-			if (a.datetime > b.datetime) {
-				return 1;
-			}
-			if (a.datetime < b.datetime) {
-				return -1;
-			}
-			// a must be equal to b
-			return 0;
-			});
+			$scope.records.sort(compareDatetime);
 			
 			console.log(data);
 			console.log(data.log);
